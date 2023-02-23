8e2c07836c6b6550ff0cd3b0a5ff818c3701326c
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2023-02-23 11:10:53 +0100

    Add a pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'my pizza test': {
+        'ingredients': {'mozzarella': 2, 'ham': 5, 'oregano': 1},
+        'price': 10,
     }
 }
